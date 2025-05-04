66cbd78369083495646b06a9614c3766b3188c78
Njdor <ryan.dunn955@gmail.com>
2025-05-03 23:28:45 -0700

    Loadcell - Jank Code But it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include <Button.h> //uh, buttons
 #include "driver/rtc_io.h" //deepsleep
+#include "hx711.h" //loadcell
 
 //GPIOs
 const int MotorPin = 13;
 const int buttonUPpin = 5;
 const int buttonDOWNpin = 4; 
+const int HX_DOUT = 10; //green wire, hx711
+const int HX_CLK = 11; //yellow wire, hx711
+#define HX711CLK GPIO_NUM_11 //define CLK this way for deepsleep pullup (hx711 in sleep)
 
 //deepsleep/wake defines, using EXT0 cause internal pullup power domain is needed.
 #define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)  // 2 ^ GPIO_NUMBER in hex
@@ -21,7 +25,15 @@ int setMotorVoltage(int, float, float);
 u_long PrevUpdateTime = 0;
 u_long SleepTimeoutTracker = 0;
 int SleepTimeoutTime = 60000; //milliseconds
+u_long MotorRunTime = 0;
+int MotorRunTimeout = 500; //milliseconds to run motor without beans
+u_long LastButtonPress = 0;
+int ButtonRunTimeout = 5000; //milliseconds to ignore motor shutoff conditions due to button press
 
+//HX711 Scale
+HX711 scale;
+float calibration_factor = -2650; //put in your calibration factor from calibration code
+float scale_reading = 0;
 
 //Button configs
 Button buttonUP = Button(buttonUPpin, BUTTON_PULLDOWN, true, 50);
@@ -97,6 +109,12 @@ void setup() {
 
   print_wakeup_reason();
 
+  //start scale
+  scale.begin(HX_DOUT, HX_CLK);
+  scale.set_scale(calibration_factor);
+  scale.tare(); //assume no beans present in slow feeder when turned on. Alternatively, can experiement with zero factor below.
+  
+
   // Assign callback functions
   //buttonUP.pressHandler(onPress);
   //buttonUP.clickHandler(onClick);
@@ -110,12 +128,18 @@ void setup() {
 
   PrevUpdateTime = millis();
   SleepTimeoutTracker = PrevUpdateTime;
-
+  
+  //quick n dirty way to avoid button press on boot screwing up calibration, should use button states to do this smartly or investigate zero factor use/stability.
+  delay(2000);
+  scale.tare();
 }
 
 void loop() {
   int updatetimebool = ((millis() - PrevUpdateTime) > MotorUpdateTime);
   if((millis() - SleepTimeoutTracker) > SleepTimeoutTime){
+      //put HX711 to sleep
+      rtc_gpio_pulldown_dis(HX711CLK);
+      rtc_gpio_pullup_en(HX711CLK);
       //begin going to sleep :)
       esp_sleep_enable_ext0_wakeup(WAKEUP_GPIO, 1);  //1 = High, 0 = Low
       // Configure pullup/downs via RTCIO to tie wakeup pins to inactive level during deepsleep.
@@ -125,13 +149,17 @@ void loop() {
       esp_deep_sleep_start();
       Serial.println("If you see this, no you didn't");
   }
-  
+  if(updatetimebool){
+    scale_reading = scale.get_units();
+    Serial.println(scale_reading, 1);
+  }
   buttonUP.process();
   buttonDOWN.process();
-  if(buttonUP.buttonstatus != 0 || buttonDOWN.buttonstatus != 0){ SleepTimeoutTracker = millis();} //delay sleep
+  if(buttonUP.buttonstatus != 0 || buttonDOWN.buttonstatus != 0){ LastButtonPress = millis();} //track last button press
+  if(buttonUP.buttonstatus != 0 || buttonDOWN.buttonstatus != 0 || scale_reading > 1.0){ SleepTimeoutTracker = millis();} //delay sleep
   if(buttonUP.buttonstatus == 1){
     //do hold things
-    Serial.println("Doing holdUP");
+    Serial.println("holdUP");
     if(updatetimebool){
       MotorVoltage = constrain(MotorVoltage + 0.2, 0, BusVoltage);
       Serial.println(MotorVoltage);
@@ -140,7 +168,7 @@ void loop() {
   }
   else if(buttonUP.buttonstatus == 2){
     //do click things
-    Serial.println("Doing clickUP");
+    Serial.println("clickUP");
     setMotorVoltage(MotorPin, BusVoltage, MotorVoltage=2.1);
     //reset buttonUP state
     buttonUP.buttonstatus = 0;
@@ -148,7 +176,7 @@ void loop() {
 
   if(buttonDOWN.buttonstatus == 1){
     //do hold things
-    Serial.println("Doing hold things");
+    Serial.println("holdDOWN");
     if(updatetimebool){
       MotorVoltage = constrain(MotorVoltage - 0.2, 0, BusVoltage);
       Serial.println(MotorVoltage);
@@ -157,12 +185,24 @@ void loop() {
   }
   else if(buttonDOWN.buttonstatus == 2){
     //do click things
-    Serial.print("Doing clickDOWN");
+    Serial.print("clickDOWN");
     setMotorVoltage(MotorPin, BusVoltage, 0);
     //reset buttonUP state
     buttonDOWN.buttonstatus = 0;
   }
   
+  if(MotorVoltage > 0 && scale_reading < 1.0 && MotorRunTime == 0){
+    MotorRunTime = millis();
+  }
+  else if(MotorVoltage > 0 && scale_reading < 1.0 && MotorRunTime > 0){
+    if((millis() - MotorRunTime > MotorRunTimeout) && (millis()-LastButtonPress > ButtonRunTimeout)){
+      setMotorVoltage(MotorPin, BusVoltage, 0);
+    }
+  }
+  else{
+    MotorRunTime = 0;
+  }
+
   if(updatetimebool){
     PrevUpdateTime = millis();
   }
